bafd6c04d37466abb6b9a47a983d24895e0d1223
Данил Зимаков <danilzimakov@MacBook-Air-Danil.local>
2024-02-06 20:13:40 +0500

    nothing

--- Final project/frontend/src/components/Navbar/Navbar.tsx
+++ Final project/frontend/src/components/Navbar/Navbar.tsx
@@ -7,19 +7,9 @@ const Navbar: React.FC = () => {
     <>
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-<<<<<<< HEAD
-          <a className="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              width="112"
-              height="28"
-            />
-          </a>
-=======
           <NavLink className="navbar-item" to={"https://bulma.io"}>
             <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
           </NavLink>
->>>>>>> dev
 
           <NavLink role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" to={''}>
             <span aria-hidden="true"></span>
@@ -30,22 +20,6 @@ const Navbar: React.FC = () => {
 
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-start">
-<<<<<<< HEAD
-            <a className="navbar-item">Home</a>
-            <a className="navbar-item">Documentation</a>
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">More</a>
-              <div className="navbar-dropdown">
-                <a className="navbar-item">About</a>
-                <a className="navbar-item">Jobs</a>
-                <a className="navbar-item">Contact</a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">Report an issue</a>
-              </div>
-            </div>
-          </div>
-          <div className="navbar-end">
-=======
             <NavLink className="navbar-item" to={'/'}>
               Home
             </NavLink>
@@ -56,7 +30,6 @@ const Navbar: React.FC = () => {
         </div>
 
         <div className="navbar-end">
->>>>>>> dev
             <div className="navbar-item">
               <div className="buttons">
                 <NavLink className="button is-primary" to={'/registration'}>
