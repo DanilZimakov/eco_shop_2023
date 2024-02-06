14f787e58cc6122dac7497f994db6ea39d87b85a
Данил Зимаков <danilzimakov@MacBook-Air-Danil.local>
2024-02-06 16:25:03 +0500

    Routes

--- Final project/backend/routes/index.routes.js
+++ Final project/backend/routes/index.routes.js
@@ -1,18 +1,7 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const mainRoute = require('./views/main.routes')
-const itemsApiRoute = require('./api/items.routes')
-const authApiRoute = require('./api/auth.routes')
-const usersApiRoute = require('./api/users.routes')
-const adminsApiRoute = require('./api/admins.routes')
-const mailApiRoute = require('./api/sendMessage.routes')
+const mainRoute = require("./views/main.routes");
 
-router.use('/', mainRoute)
+router.use("/", mainRoute);
 
-router.use('/api/items', itemsApiRoute)
-router.use('/api/auth', authApiRoute)
-router.use('/api/users', usersApiRoute)
-router.use('/api/admins', adminsApiRoute)
-router.use('/api/sendMessage', mailApiRoute)
-
-module.exports = router
+module.exports = router;
