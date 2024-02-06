755d592713d19ee7a70f3b75662a021faef81b7a
Yuliya Filippova <filippovayn1994@gmail.com>
2024-02-06 16:06:15 +0300

    navbar

--- Final project/frontend/src/components/Navbar/Navbar.tsx
+++ Final project/frontend/src/components/Navbar/Navbar.tsx
@@ -1,67 +1,50 @@
-import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <>
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              width="112"
-              height="28"
-              alt="Bulma Logo"
-            />
-          </a>
+          <NavLink className="navbar-item" to={"https://bulma.io"}>
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+          </NavLink>
 
-          <a
-            role="button"
-            className="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
+          <NavLink role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" to={''}>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
-          </a>
+          </NavLink>
         </div>
 
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-start">
-            <a className="navbar-item">Home</a>
+            <NavLink className="navbar-item" to={'/'}>
+              Home
+            </NavLink>
+            <NavLink className="navbar-item" to={'/categories'}>
+              Categories
+            </NavLink>
 
-            <a className="navbar-item">Documentation</a>
+        </div>
 
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">More</a>
-
-              <div className="navbar-dropdown">
-                <a className="navbar-item">About</a>
-                <a className="navbar-item">Jobs</a>
-                <a className="navbar-item">Contact</a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">Report an issue</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="navbar-end">
+        <div className="navbar-end">
             <div className="navbar-item">
               <div className="buttons">
-                <a className="button is-primary">
+                <NavLink className="button is-primary" to={'/registration'}>
                   <strong>Sign up</strong>
-                </a>
-                <a className="button is-light">Log in</a>
+                </NavLink>
+                <NavLink className="button is-light" to={'/login'}>
+                  Log in
+                </NavLink>
               </div>
             </div>
           </div>
         </div>
       </nav>
-      <Outlet />
     </>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
