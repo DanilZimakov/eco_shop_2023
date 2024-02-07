fc171e9e252cf9b145a828b70288a1e7587d9681
Данил Зимаков <danilzimakov@MacBook-Air-Danil.local>
2024-02-07 15:04:11 +0500

    Снова ругался на импорт React в Navbar

--- Final project/frontend/src/components/Navbar/Navbar.tsx
+++ Final project/frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom"; // Переместите оператор импорта сюда
 import "./logo_1.svg";
+
 const Navbar = () => {
   return (
     <>
