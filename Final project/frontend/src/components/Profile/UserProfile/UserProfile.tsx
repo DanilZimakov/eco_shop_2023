aafb086ab7d1b02362e01bcf4ec1be5aaef11eb5
Данил Зимаков <danilzimakov@MacBook-Air-Danil.local>
2024-02-08 12:44:52 +0500

    нет изминений

--- Final project/frontend/src/components/Profile/UserProfile/UserProfile.tsx
+++ Final project/frontend/src/components/Profile/UserProfile/UserProfile.tsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-const UserProfile = () => {
-  return <div>UserProfile</div>;
-};
-=======
-import React, { useState } from "react";
-import './UserProfile.css';
-import AddForm from '../AddForm/AddForm';
+import { useState } from "react";
+import "./UserProfile.css";
+import AddForm from "../AddForm/AddForm";
 
 function UserProfile(): JSX.Element {
   const [rating, setRating] = useState(0);
@@ -14,15 +9,26 @@ function UserProfile(): JSX.Element {
     setRating(newRating);
   };
 
-  const handleFormSubmit = (data: { productName: string, productType: string, gender: string, compositions: Array<{ material: string, quantity: number }> }) => {
-    console.log('Форма отправлена с данными:', data);
+  const handleFormSubmit = (data: {
+    productName: string;
+    productType: string;
+    gender: string;
+    compositions: Array<{ material: string; quantity: number }>;
+  }) => {
+    console.log("Форма отправлена с данными:", data);
     // Здесь можно обработать данные формы, например, отправить их на сервер
   };
 
   return (
     <>
       <div className="user-profile">
-        <img src={'https://cdn.fishki.net/upload/post/2018/04/09/2564770/1-4d137341-2db8-49c6-83de-e8ef75519d43.jpg'} alt="User avatar" className="user-avatar" />
+        <img
+          src={
+            "https://cdn.fishki.net/upload/post/2018/04/09/2564770/1-4d137341-2db8-49c6-83de-e8ef75519d43.jpg"
+          }
+          alt="User avatar"
+          className="user-avatar"
+        />
         <h2 className="user-name">Name</h2>
         <div className="user-info">
           <h5 className="user-info details">Инфо о пользователе</h5>
@@ -33,8 +39,8 @@ function UserProfile(): JSX.Element {
               {[...Array(5)].map((_, i) => (
                 <span
                   key={i}
-                  className={`star ${i < rating ? 'full' : ''}`}
-                  onClick={() => handleRatingChange(i +  1)}
+                  className={`star ${i < rating ? "full" : ""}`}
+                  onClick={() => handleRatingChange(i + 1)}
                 >
                   ★
                 </span>
@@ -43,13 +49,11 @@ function UserProfile(): JSX.Element {
           </div>
         </div>
         <div className="add">
-        <AddForm onSubmit={handleFormSubmit} />
+          <AddForm onSubmit={handleFormSubmit} />
         </div>
       </div>
-
     </>
   );
 }
->>>>>>> 82c6cee093577a721013c851c73c200a64cb7373
 
 export default UserProfile;
