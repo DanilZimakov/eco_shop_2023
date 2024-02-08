66bc297aa1ee13cd2d930c40a5d7ca382ff76350
Yuliya Filippova <filippovayn1994@gmail.com>
2024-02-08 10:24:24 +0300

    userprofile

--- Final project/frontend/src/components/Profile/UserProfile/UserProfile.tsx
+++ Final project/frontend/src/components/Profile/UserProfile/UserProfile.tsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-const UserProfile = () => {
-  return <div>UserProfile</div>;
-};
-=======
 import React, { useState } from "react";
 import './UserProfile.css';
 import AddForm from '../AddForm/AddForm';
@@ -50,6 +45,5 @@ function UserProfile(): JSX.Element {
     </>
   );
 }
->>>>>>> 82c6cee093577a721013c851c73c200a64cb7373
 
 export default UserProfile;
