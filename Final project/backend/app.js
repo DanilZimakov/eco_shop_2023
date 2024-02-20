a03c6b3be9439e3ebbfd6685f004733dea8402ae
Данил Зимаков <danilzimakov@MacBook-Air-Danil.local>
2024-02-20 14:24:33 +0500

    поблема с CORS

--- Final project/backend/app.js
+++ Final project/backend/app.js
@@ -3,12 +3,12 @@ const serverConfig = require("./config/serverConfig");
 const indexRouter = require("./routes/index.routes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 serverConfig(app);
 
 app.use("/", indexRouter);
 
 app.listen(PORT, () => {
-  console.log("This server is dying on port: ", PORT);
+  console.log(`Server running on port: ${PORT}`);
 });
--- Final project/backend/config/serverConfig.js
+++ Final project/backend/config/serverConfig.js
@@ -4,13 +4,18 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const corsOption = {
+  origin: process.env.APP_DOMAIN,
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
 const serverConfig = (app) => {
   app.use(express.json({ limit: "200mb" }));
   app.use(express.urlencoded({ limit: "200mb", extended: true }));
   app.use(express.text({ limit: "200mb" }));
-  app.use(express.json());
-  app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-  app.use(cookieParser());
+  app.use(cors());
+  app.use(cookieParser(corsOption));
 };
 
 module.exports = serverConfig;
