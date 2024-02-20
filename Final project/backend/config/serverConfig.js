a0c94ef366b4423a9cf0106f8a466b13fb7923a6
Данил Зимаков <danilzimakov@MacBook-Air-Danil.local>
2024-02-20 15:07:07 +0500

    снова проболема с CORS   app.use(cors(corsOption));

--- Final project/backend/config/serverConfig.js
+++ Final project/backend/config/serverConfig.js
@@ -14,8 +14,8 @@ const serverConfig = (app) => {
   app.use(express.json({ limit: "200mb" }));
   app.use(express.urlencoded({ limit: "200mb", extended: true }));
   app.use(express.text({ limit: "200mb" }));
-  app.use(cors());
-  app.use(cookieParser(corsOption));
+  app.use(cors(corsOption));
+  app.use(cookieParser());
 };
 
 module.exports = serverConfig;
