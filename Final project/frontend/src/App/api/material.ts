ba1dcf639c31337cdd4440f5ef7547d74642772d
Данил Зимаков <danilzimakov@MacBook-Air-Danil.local>
2024-02-21 23:04:13 +0500

    ни чего не менял

--- Final project/frontend/src/App/api/material.ts
+++ Final project/frontend/src/App/api/material.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { MaterialType } from "../../types/materials/materials";
 
-const BASE_URL:string = "https://swap-style-eco.shop";
-// const BASE_URL: string = "http://localhost:3000";
+// const BASE_URL:string = "https://swap-style-eco.shop";
+const BASE_URL: string = "http://localhost:3000";
 
 export const axiosMaterialsLoad = async (): Promise<MaterialType[]> => {
   const res = await axios.get(`${BASE_URL}/materials`);
