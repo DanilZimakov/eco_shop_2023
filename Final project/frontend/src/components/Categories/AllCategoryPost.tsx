a3e46f9df7e363dd3aaf4a66cdccc6ef0f6b07a0
Nimaev-Valeriy <nimval@mail.ru>
2024-02-21 21:52:03 +0800

    fixed AllCategory

--- Final project/frontend/src/components/Categories/AllCategoryPost.tsx
+++ Final project/frontend/src/components/Categories/AllCategoryPost.tsx
@@ -1,15 +1,60 @@
 import { useSelector } from "react-redux";
-import { RootState } from "../../redux/store";
+import { RootState, useAppDispatch } from "../../redux/store";
 import { useParams } from "react-router-dom";
 import { PostType } from "../../types/posts/posts";
 import LikeButton from "../LikeButton/LikeButton";
 import { HarmType } from "../../types/harm/harm";
+import { addItem } from "../../redux/Slice/cartSlice/cartSlice";
+import cart from "./cart.png"
+import axios from "axios";
 
 function AllCategoryPost() {
     const { categoryId } = useParams();
     const { posts } = useSelector((s:RootState) => s.posts)
     const fillPost = posts.filter((el:PostType) => el.category_id === Number(categoryId))
     const {harm} = useSelector((s:RootState) => s.harm)
+    const dispatch = useAppDispatch()
+    const handleAddClick = async (post: PostType) => {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          console.error("Authentication token or user id is missing.");
+          return;
+        }
+
+        const response = await axios.post(
+          `http://localhost:3000/cart/add`,
+          { post_id: post.id, quantity: 1 },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        console.log("Item added to cart:", response.data);
+        dispatch(
+          addItem({
+            ...post,
+            quantity: 1,
+            post: {
+              id: 0,
+              name: "",
+              price: 0,
+              description: "",
+              image: "",
+              size: "",
+              publich: false,
+              user_id: 0,
+              category_id: 0,
+              sub_category_id: 0,
+            },
+          })
+        );
+      } catch (error) {
+        console.error("Error adding item to cart:", error);
+      }
+    };
     return (
       <div className="product-card">
         {fillPost.map((post: PostType) => {
@@ -33,14 +78,14 @@ function AllCategoryPost() {
               <p>Экологическая оценка: {harmSearch?.ecoStatus}</p>
               <div>
                 <LikeButton postId={post.id} categoryId={Number(categoryId)} />
-                {/* <div className="add ">
+                <div className="add ">
                 <img
                   className="a"
                   src={cart}
                   alt="Add"
                   onClick={() => handleAddClick(post)}
                 />
-              </div> */}
+              </div>
               </div>
             </div>
           );
