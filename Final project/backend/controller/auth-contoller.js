26d6e2bdb5dca4ad34323a615bec7facadc1d5ee
Данил Зимаков <danilzimakov@MacBook-Air-Danil.local>
2024-02-22 20:35:28 +0500

    dfdkfhkd

--- Final project/backend/controller/auth-contoller.js
+++ Final project/backend/controller/auth-contoller.js
@@ -27,7 +27,7 @@ class AuthController {
         phone_number: phone,
         is_admin: false,
       });
-      //   sendMail(email);
+      sendMail(email);
       const userData = {
         id: user.id,
         name: user.name,
