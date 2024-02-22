5d082b48a762a00b566fb6eb575565c7dee3ee39
Данил Зимаков <danilzimakov@MacBook-Air-Danil.local>
2024-02-22 17:10:53 +0500

    dkdfkd

--- Final project/frontend/src/App/api/category.ts
+++ Final project/frontend/src/App/api/category.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 
 export const fetchCategories = async () => {
   try {
-    const response = await axios.get(
-      "https://swap-style-eco.shop",
-    );
+    const response = await axios.get("https://swap-style-eco.shop");
     return response.data;
   } catch (error) {
     console.error(error);
